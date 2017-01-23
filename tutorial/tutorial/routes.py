8caf2342efb910c9d60088051fde3f4fa0b14131
sudheeshna <sudheeshnakotte@gmail.com>
2017-01-23 21:48:26 +0530

    Update routes.py

--- tutorial/tutorial/routes.py
+++ tutorial/tutorial/routes.py
@@ -1,4 +1,5 @@
 def includeme(config):
+    
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('view_wiki', '/')
     config.add_route('view_page', '/{pagename}')
